0f1114f2fbfe98304585004821cb9b84a38318e6
josefinaPorolli <j.porolli@alumno.um.edu.ar>
2025-04-29 22:18:30 -0300

    Etapa 2: implementación de función recursiva de fibonacci

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -1,7 +1,19 @@
+# Se crean dos funciones para calcular la serie de Fibonacci. Se introduce la posición y se muestra el número correspondiente a esa posición.
 def fibonacci_iterativo(n):
-    sum = 0
-    a = 1
-    while a <= n:
-        sum += a
-        a += 1
-    return sum
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
+def fibonacci_recursivo(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursivo(n - 1) + fibonacci_recursivo(n - 2)
